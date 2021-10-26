1441ff8cc0f2e0fd5b953718f0c1550494714056
uniudlabrobotica <92531923+uniudlabrobotica@users.noreply.github.com>
2021-10-26 10:37:54 +0200

    Add files via upload

--- prepare_script.py
+++ prepare_script.py
@@ -8,7 +8,7 @@ import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
 from math import pi
-
+import copy
 import std_msgs.msg
 from franka_gripper.msg import MoveActionGoal
 from franka_msgs.msg import FrankaState
@@ -18,14 +18,11 @@ from time import sleep
 
 height_block=.02
 width_block=.02
-z_base=.082
 
-#x_position=[.54, .54, .54]
-#y_position=[-.27, -.03, .24]
 
-central_position=[2.09, -1.16, -2.11, -2, -1.33, 2.20, -1.68]
-base_joint=[2.6, 2.10, 2.14]
-
+y_position = [-.04, 0, .04]
+x_position = 0.1
+z_up = .1
 
 def getActions():
     os.system("clingo toAsp.lp | tail -10 | head -1 > actions.txt")
@@ -48,69 +45,114 @@ def getActions():
         actionList.append(arguments)
     return actionList
 
-def execute_actions(move_group, pub, actions):
+def execute_actions(move_group, pub, display_trajectory_publisher, actions):
 
     for action in actions:
-        
-        #go_to_init_goal(move_group)
-        #go_to_pose_goal(move_group, action)
-        move_robot(move_group, action)
+        print(action)
+        print("move left/right")
+        move_robot(move_group, display_trajectory_publisher, action)
+        print(move_group.get_current_pose().pose)
         sleep(2)
+        print("let down")
+        let_down(move_group, display_trajectory_publisher, action)
+        print(move_group.get_current_pose().pose)
+        sleep(2)
+        print("move gripper")
         move_gripper(pub, action[3])
+        print(move_group.get_current_pose().pose)
+        sleep(2)
+        print("raise up")
+        raise_up(move_group, display_trajectory_publisher)
+        print(move_group.get_current_pose().pose)
+        sleep(2)
+
+def raise_up(move_group, display_trajectory_publisher):
+
+    waypoints = []
+
+    wpose = move_group.get_current_pose().pose
+    wpose.position.z = z_up
+
+    waypoints.append(copy.deepcopy(wpose))
+
+    (plan, fraction) = move_group.compute_cartesian_path(
+                                       waypoints,   # waypoints to follow
+                                       0.01,        # eef_step
+                                       0.0)         # jump_threshold
+
+    display_trajectory = moveit_msgs.msg.DisplayTrajectory()
+    display_trajectory.trajectory_start = robot.get_current_state()
+    display_trajectory.trajectory.append(plan)
+    # Publish
+    display_trajectory_publisher.publish(display_trajectory)
+
+    move_group.execute(plan, wait=True)
+
+def let_down(move_group, display_trajectory_publisher, action):
+
+    pose_goal = move_group.get_current_pose().pose
+
+    valy=y_position[int(action[3])-1]
+
+    pose_goal.position.z = (int(action[3])-1)*height_block+z_up
+
+    move_group.set_pose_target(pose_goal)
+    plan = move_group.go(wait=True)
+    move_group.stop()
+    move_group.clear_pose_targets()
+    current_pose = move_group.get_current_pose().pose
 
 
-def go_to_init_goal(move_group):
-    pose_goal = geometry_msgs.msg.Pose()
-    pose_goal.orientation.w = 0.0227707401509
-    pose_goal.orientation.x = 0.383262744613
-    pose_goal.orientation.y = 0.923320337876
-    pose_goal.orientation.z = 0.00840925977722
-    pose_goal.position.x = .5
+
+def go_to_init_goal(move_group, display_trajectory_publisher):
+    waypoints = []
+    pose_goal = move_group.get_current_pose().pose
+    pose_goal.orientation.x = 0.923955699469
+    pose_goal.orientation.y = -0.382499497279
+    pose_goal.orientation.z = 1.3249325839e-12
+    pose_goal.orientation.w = 3.20041176635e-12
+    pose_goal.position.x = 0.2
     pose_goal.position.y = 0
-    pose_goal.position.z = .5
+    pose_goal.position.z = z_up
+    waypoints.append(copy.deepcopy(pose_goal))
+    (plan, fraction) = move_group.compute_cartesian_path(
+                                       waypoints,   # waypoints to follow
+                                       0.01,        # eef_step
+                                       0.0)         # jump_threshold
 
-    move_group.set_pose_target(pose_goal)
+    display_trajectory = moveit_msgs.msg.DisplayTrajectory()
+    display_trajectory.trajectory_start = robot.get_current_state()
+    display_trajectory.trajectory.append(plan)
+    # Publish
+    display_trajectory_publisher.publish(display_trajectory)
 
-    plan = move_group.go(wait=True)
-    move_group.stop()
-    move_group.clear_pose_targets()
+    move_group.execute(plan, wait=True)
 
-    current_pose = move_group.get_current_pose().pose
-    return (pose_goal, current_pose, 0.01)
 
-def move_robot(move_group, action):
+def move_robot(move_group, display_trajectory_publisher, action):
 
-    joint_goal = move_group.get_current_joint_values()
-    joint_goal=[2.09, -1.16, -2.11, -2, -1.33, 2.20, -1.68]
-    val=0
-    if int(action[3])==1:
-        val=-.08
-    elif int(action[3])==2:
-        val=-.08
-    else :
-        val=.08
-    joint_goal[0] = joint_goal[0]+val
-    move_group.go(joint_goal, wait=True)
-    move_group.stop()
-    current_joints = move_group.get_current_joint_values()
+    waypoints = []
 
-def go_to_pose_goal(move_group, action):
+    wpose = move_group.get_current_pose().pose
 
-    current_pose = move_group.get_current_pose().pose
-    pose_goal = geometry_msgs.msg.Pose()
-    print(action)
-    pose_goal.position.x = x_position[int(action[3])-1]
-    pose_goal.position.y = y_position[int(action[3])-1]
-    pose_goal.position.z = 0.02+z_base+(int(action[2])-1)*height_block
+    wpose.position.y = y_position[int(action[3])-1]
 
-    move_group.set_pose_target(pose_goal)
+    waypoints.append(copy.deepcopy(wpose))
 
-    plan = move_group.go(wait=True)
-    move_group.stop()
-    move_group.clear_pose_targets()
+    (plan, fraction) = move_group.compute_cartesian_path(
+                                       waypoints,   # waypoints to follow
+                                       0.01,        # eef_step
+                                       0.0)         # jump_threshold
 
-    current_pose = move_group.get_current_pose().pose
-    return (pose_goal, current_pose, 0.01)
+    display_trajectory = moveit_msgs.msg.DisplayTrajectory()
+    display_trajectory.trajectory_start = robot.get_current_state()
+    display_trajectory.trajectory.append(plan)
+    # Publish
+    display_trajectory_publisher.publish(display_trajectory)
+
+    move_group.execute(plan, wait=True)
+
+
 
 def move_gripper(pub, type_action):
     
@@ -133,18 +175,66 @@ if __name__ == "__main__":
     group_name = "panda_arm"
     move_group = moveit_commander.MoveGroupCommander(group_name)
 
+    print("before table")
+    box_pose = geometry_msgs.msg.PoseStamped()
+    box_pose.header.frame_id = "panda_leftfinger"
+    box_pose.pose.position.z = 0
+    box_pose.pose.position.x = 0
+    box_name = "table"
+    scene.add_box(box_name, box_pose, size=(3, 3, .02))
+    print("after table")
+
+    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                               moveit_msgs.msg.DisplayTrajectory,
+                                               queue_size=20)
+
     pub = rospy.Publisher('franka_gripper/move/goal', MoveActionGoal, queue_size=10)
-    go_to_init_goal(move_group)
-    execute_actions(move_group, pub, l)
+    print("got to starting position")
+    go_to_init_goal(move_group, display_trajectory_publisher)
+    execute_actions(move_group, pub, display_trajectory_publisher, l)
+    print("got to starting position")
+    go_to_init_goal(move_group, display_trajectory_publisher)
 
-
-"""
-  x: 0.54522191883
-  y: -0.0316979921766
-  z: 0.0836985635194
-"""
 
 
 """
-[2.09, -1.16, -2.11, -2, -1.33, 2.20, -1.68]
+def raise_up(move_group):
+    pose_goal = move_group.get_current_pose().pose
+    pose_goal.position.z = z_up
+    move_group.set_pose_target(pose_goal)
+    plan = move_group.go(wait=True)
+    move_group.stop()
+    move_group.clear_pose_targets()
+    current_pose = move_group.get_current_pose().pose
+
+
+def let_down(move_group, action):
+    pose_goal = move_group.get_current_pose().pose
+
+    valy=y_position[int(action[3])-1]
+
+    pose_goal.position.z = 0.04+(int(action[3])-1)*height_block+z_up
+
+    move_group.set_pose_target(pose_goal)
+    plan = move_group.go(wait=True)
+    move_group.stop()
+    move_group.clear_pose_targets()
+    current_pose = move_group.get_current_pose().pose
+
+
+def move_robot(move_group, action):
+    pose_goal = move_group.get_current_pose().pose
+
+    valy=y_position[int(action[3])-1]
+
+    pose_goal.position.y = valy
+    
+    move_group.set_pose_target(pose_goal)
+
+    plan = move_group.go(wait=True)
+    move_group.stop()
+    move_group.clear_pose_targets()
+
+    current_pose = move_group.get_current_pose().pose
+    return (pose_goal, current_pose, 0.01)
 """
